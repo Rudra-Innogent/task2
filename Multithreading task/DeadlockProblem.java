1eb344d4debd15dac6dd79889f8bdaa05b94ab98
Rudra-Thakur-007 <rrrudra007sh@gmail.com>
2025-10-09 12:55:19 +0530

    DeadLock Problem Solution Example

--- Multithreading task/DeadlockProblem.java
+++ Multithreading task/DeadlockProblem.java
@@ -1,66 +1,77 @@
 //classic deadlock condition example
-class A{
-A(){}
-	void show1(B b1){
-		System.out.println("class A show1 method");
-		b1.show2();
-	}
-	void show2(){
-		System.out.println("class A show method");
-	}
+class A {
+    A() {}
+    void show1(B b1) {
+        System.out.println(Thread.currentThread().getName() + " entered A.show1() and trying to call B.show2()");
+        b1.show2();
+    }
+    void show2() {
+        System.out.println(Thread.currentThread().getName() + " executing A.show2()");
+    }
 }
-class B{
-B(){}
-	void show1(A a1){
-		System.out.println("class B show1 method");
-		a1.show2();
-	}
-	void show2(){
-		System.out.println("class B show method");
-	}
+class B {
+    B() {}
+    void show1(A a1) {
+        System.out.println(Thread.currentThread().getName() + " entered B.show1() and trying to call A.show2()");
+        a1.show2();
+    }
+    void show2() {
+        System.out.println(Thread.currentThread().getName() + " executing B.show2()");
+    }
 }
-class C extends Thread{
-	A a1;
-	B b1;
-	C(A a1,B b1){
-		this.a1=a1;
-		this.b1=b1;
-	}
-	public void run(){
-		synchronized(a1){//locking a1 object
-			a1.show1(b1);
-			synchronized(b1){//locking b1 object
-				b1.show1(a1);
-			}
-		}
-	}
+class C extends Thread {
+    A a1;
+    B b1;
+    C(A a1, B b1) {
+        this.a1=a1;
+        this.b1=b1;
+    }
+    public void run() {
+        this.setName("Thread-C");
+        System.out.println(getName()+" started and trying to lock resource: A");
+        synchronized (a1) {
+            System.out.println(getName()+" locked resource: A");
+            try { Thread.sleep(100); } catch (InterruptedException e) {}
+            System.out.println(getName()+" now trying to lock resource: B");
+            synchronized (b1) {
+                System.out.println(getName()+" locked resource: B");
+                b1.show1(a1);
+                System.out.println(getName()+" finished using resources A & B");
+            }
+        }
+    }
 }
 
-class D extends Thread{
-	A a1;
-	B b1;
-	D(A a1,B b1){
-		this.a1=a1;
-		this.b1=b1;
-	}
-	public void run(){
-		synchronized(b1){//locking b1 object
-			b1.show1(a1);
-			synchronized(a1){//locking a1 object
-				a1.show1(b1);
-			}
-		}
-	}
+class D extends Thread {
+    A a1;
+    B b1;
+    D(A a1, B b1) {
+        this.a1=a1;
+        this.b1=b1;
+    }
+    public void run() {
+        this.setName("Thread-D");
+        System.out.println(getName()+" started and trying to lock resource: B");
+        synchronized (b1) {
+            System.out.println(getName()+" locked resource: B");
+            try { Thread.sleep(100); } catch (InterruptedException e) {}
+            System.out.println(getName()+" now trying to lock resource: A");
+            synchronized (a1) {
+                System.out.println(getName()+" locked resource: A");
+                a1.show1(b1);
+                System.out.println(getName()+" finished using resources B & A");
+            }
+        }
+    }
 }
 class DeadlockProblem {
     public static void main(String[] args) {
-		A a=new A();
-		B b=new B();
-		C c=new C(a,b);//passing a1 and b1 this way if C-thread runs first it will lock a1 and b1 object respectively
-		D d=new D(a,b);// passing same a1 and b1 object, D-thread runs first it will lock a1 and b1 objects respectively  
-		c.start();
-	    d.start();
-		System.out.println("main method");
-		
-	}
-	}
+        A a = new A();
+        B b = new B();
+        C c = new C(a, b);
+        D d = new D(a, b);
+        c.start();
+        d.start();
+        System.out.println("main method");
+    }
+}
---
+++ Multithreading task/DeadlockSolution.java
@@ -0,0 +1,127 @@
+//Solution of Deadlock using Lock interface and its implemented class ReentrantLock's methid tryLock(time,TimeUnit)
+import java.util.concurrent.locks.*;
+import java.util.concurrent.TimeUnit;
+class A{
+A(){}
+	void show1(B b1){ 
+	System.out.println(Thread.currentThread().getName()+" class B show1 method"); 
+	b1.show2(); 
+	} 
+	void show2(){ 
+	System.out.println(Thread.currentThread().getName()+" class B show2 method"); 
+	}
+}
+class B{
+B(){}
+	void show1(A a1){ 
+	System.out.println(Thread.currentThread().getName()+" class B show1 method"); 
+	a1.show2(); 
+	} 
+	void show2(){ 
+	System.out.println(Thread.currentThread().getName()+" class B show2 method"); 
+	}
+}
+class C extends Thread {
+    Lock lock;
+    A a1;
+    B b1;
+
+    C(A a1, B b1, Lock lock) {
+        this.a1=a1;
+        this.b1=b1;
+        this.lock=lock;
+    }
+    public void run() {
+        this.setName("Thread-C");
+        boolean firstLock=false;
+        boolean secondLock=false;
+        try {
+            firstLock = lock.tryLock(1, TimeUnit.SECONDS);
+            if (firstLock) {
+                System.out.println(getName() + " acquired lock - performing A.show1()");
+                a1.show1(b1);
+                secondLock = lock.tryLock(1, TimeUnit.SECONDS);
+                if (secondLock) {
+                    System.out.println(getName() + " acquired lock again - performing B.show1()");
+                    b1.show1(a1);
+                } else {
+                    System.out.println(getName() + " could not re-acquire lock (timeout) - skipping B.show1()");
+                }
+            } else {
+                System.out.println(getName() + " could not acquire lock (timeout) - skipping work");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (secondLock) {
+                lock.unlock();
+                System.out.println(getName() + " released second lock");
+            }
+            if (firstLock) {
+                lock.unlock();
+                System.out.println(getName() + " released first lock");
+            }
+        }
+    }
+}
+class D extends Thread {
+    A a1;
+    B b1;
+    Lock lock;
+    D(A a1, B b1, Lock lock) {
+        this.a1=a1;
+        this.b1=b1;
+        this.lock=lock;
+    }
+    public void run() {
+        this.setName("Thread-D");
+
+        boolean firstLock=false;
+        boolean secondLock=false;
+
+        try {
+            // Try to acquire first lock
+            firstLock = lock.tryLock(1, TimeUnit.SECONDS);
+            if (firstLock) {
+                System.out.println(getName() + " acquired lock - performing B.show1()");
+                b1.show1(a1);
+
+                // Try to acquire lock again (reentrant)
+                secondLock = lock.tryLock(1, TimeUnit.SECONDS);
+                if (secondLock) {
+                    System.out.println(getName() + " acquired lock again - performing A.show1()");
+                    a1.show1(b1);
+                } else {
+                    System.out.println(getName() + " could not re-acquire lock (timeout) - skipping A.show1()");
+                }
+            } else {
+                System.out.println(getName() + " could not acquire lock (timeout) - skipping work");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (secondLock) {
+                lock.unlock();
+                System.out.println(getName() + " released second lock");
+            }
+            if (firstLock) {
+                lock.unlock();
+                System.out.println(getName() + " released first lock");
+            }
+        }
+    }
+}
+
+class DeadlockSolution {
+    public static void main(String[] args) {
+		Lock lock=new ReentrantLock();
+		A a=new A();
+		B b=new B();
+		C c=new C(a,b,lock);//passing a1 and b1 this way if C-thread runs first it will lock a1 and b1 object respectively
+		D d=new D(a,b,lock);// passing same a1 and b1 object, D-thread runs first it will lock a1 and b1 objects respectively  
+		c.start();
+	    d.start();
+		System.out.println("main method");
+		
+	}
+	}
