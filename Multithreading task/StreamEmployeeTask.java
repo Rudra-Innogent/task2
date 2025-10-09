8efd6cbf0f7d1dde0fd7964cfb83ef55b684e1f5
Rudra-Thakur-007 <rrrudra007sh@gmail.com>
2025-10-09 16:08:57 +0530

    Updated Directory for Stream and Semaphore task

--- Multithreading task/StreamEmployeeTask.java
+++ Multithreading task/StreamEmployeeTask.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.util.stream.Collectors;
-public class Employee {
+public class StreamEmployeeTask {
     int id;
     String name;
     String department;
     int salary;
 
-    public Employee(int id, String name, String department, int salary) {
+    public StreamEmployeeTask(int id, String name, String department, int salary) {
         this.id = id;
         this.name = name;
         this.department = department;
@@ -40,7 +40,7 @@ public class Employee {
 
     @java.lang.Override
     public java.lang.String toString() {
-        return "Employee{" +
+        return "StreamEmployeeTask{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", department='" + department + '\'' +
@@ -49,17 +49,17 @@ public class Employee {
     }
 	
 	public static void main(String ar[]){
-		Employee e1=new Employee(101,"Rajni","HR",60000);
-		Employee e2=new Employee(102,"Jyoti","Management",80000);
-		Employee e3=new Employee(103,"Raghav","HR",45000);
-		Employee e4=new Employee(104,"Rudra","Development",60000);
-		Employee e5=new Employee(105,"Saurabh","Management",50000);
-		Employee e6=new Employee(106,"Arun","HR",65000);
-		List<Employee> list = Arrays.asList(e1,e2,e3,e4,e5,e6);
-		List<Employee> deptFilter = list.stream().filter((a)->a.department.equals("HR")).collect(Collectors.toList());
+		StreamEmployeeTask e1=new StreamEmployeeTask(101,"Rajni","HR",60000);
+		StreamEmployeeTask e2=new StreamEmployeeTask(102,"Jyoti","Management",80000);
+		StreamEmployeeTask e3=new StreamEmployeeTask(103,"Raghav","HR",45000);
+		StreamEmployeeTask e4=new StreamEmployeeTask(104,"Rudra","Development",60000);
+		StreamEmployeeTask e5=new StreamEmployeeTask(105,"Saurabh","Management",50000);
+		StreamEmployeeTask e6=new StreamEmployeeTask(106,"Arun","HR",65000);
+		List<StreamEmployeeTask> list = Arrays.asList(e1,e2,e3,e4,e5,e6);
+		List<StreamEmployeeTask> deptFilter = list.stream().filter((a)->a.department.equals("HR")).collect(Collectors.toList());
 		deptFilter.forEach(System.out::println);
 		System.out.println();
-		List<Employee> nameFilter = list.stream().map((a)->new Employee(a.id,a.name.toUpperCase(),a.department,a.salary)).collect(Collectors.toList());
+		List<StreamEmployeeTask> nameFilter = list.stream().map((a)->new StreamEmployeeTask(a.id,a.name.toUpperCase(),a.department,a.salary)).collect(Collectors.toList());
 		nameFilter.forEach(System.out::println);
 		System.out.println();
 		Integer salarySum = list.stream().map(a->a.salary).reduce(0,(a,b)-> a + b );
